db2b544c3f72c03a1fa541d0a9ed59026e02942b
diego <diego@nutmeg.com>
2018-06-06 18:01:51 +0100

    Update Port

--- packages/react-scripts/scripts/start.js
+++ packages/react-scripts/scripts/start.js
@@ -47,8 +47,8 @@ if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
   process.exit(1);
 }
 
-// Tools like Cloud9 rely on this.
-const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 3000;
+// Tools like Cloud9 rely on this. @nutkit Update port to don't crash with auth
+const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 4000;
 const HOST = process.env.HOST || '0.0.0.0';
 
 if (process.env.HOST) {
